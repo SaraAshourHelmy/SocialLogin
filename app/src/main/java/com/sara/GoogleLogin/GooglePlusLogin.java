d439081ecf78be8a243981074cee5189ae080e3c
bassem3312 <basem3312@gmail.com>
2016-08-21 12:53:34 +0200

    Initial commit

---
+++ app/src/main/java/com/sara/GoogleLogin/GooglePlusLogin.java
@@ -0,0 +1,7 @@
+package com.sara.GoogleLogin;
+
+/**
+ * Created by Bassem on 8/21/2016.
+ */
+public class GooglePlusLogin {
+}
---
+++ app/src/main/java/com/sara/TwitterLogin/TwitterLogin.java
@@ -0,0 +1,7 @@
+package com.sara.TwitterLogin;
+
+/**
+ * Created by Bassem on 8/21/2016.
+ */
+public class TwitterLogin {
+}
